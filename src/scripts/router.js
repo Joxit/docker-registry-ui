a3276dcb79902a8fa617a47b13f17f15c370c72b
Joxit <joxit972@gmail.com>
2021-03-20 08:15:05 +0100

    feat(riot-v5): upgrade all tag-history components

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016-2021 Jones Magloire @Joxit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import { router, getCurrentRoute } from '@riotjs/route';
 
 function baseUrl() {
@@ -13,5 +29,14 @@ export default {
   },
   getTagListImage() {
     return getCurrentRoute().replace(/^.*(#!)?\/?taglist\//, '');
-  }
+  },
+  history(image, tag) {
+    router.push(`${baseUrl()}#!/taghistory/image/${image}/tag/${tag}`);
+  },
+  getTagHistoryImage() {
+    return getCurrentRoute().replace(/^.*(#!)?\/?taghistory\/image\/(.*)\/tag\/(.*)\/?$/, '$2');
+  },
+  getTagHistoryTag() {
+    return getCurrentRoute().replace(/^.*(#!)?\/?taghistory\/image\/(.*)\/tag\/(.*)\/?$/, '$3');
+  },
 };

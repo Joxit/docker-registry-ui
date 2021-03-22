781b0264719a5f0399c638ca80d2b5e82a11effc
Joxit <joxit972@gmail.com>
2021-03-22 07:59:35 +0100

    fix(riot-v5): on page change, selected images for deletion should be cleared
    update riot-mui version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { component, register } from 'riot';
-import MaterialCard from 'riot-mui/src/material-elements/material-card/material-card.riot';
-import MaterialSpinner from 'riot-mui/src/material-elements/material-spinner/material-spinner.riot';
-import MaterialNavbar from 'riot-mui/src/material-elements/material-navbar/material-navbar.riot';
-import MaterialFooter from 'riot-mui/src/material-elements/material-footer/material-footer.riot';
-import MaterialButton from 'riot-mui/src/material-elements/material-button/material-button.riot';
-import MaterialCheckbox from 'riot-mui/src/material-elements/material-checkbox/material-checkbox.riot';
-import MaterialTabs from 'riot-mui/src/material-elements/material-tabs/material-tabs.riot';
+import {
+  MaterialCard,
+  MaterialSpinner,
+  MaterialNavbar,
+  MaterialFooter,
+  MaterialButton,
+  MaterialCheckbox,
+  MaterialTabs,
+} from 'riot-mui';
 
 import DockerRegistryUI from './components/docker-registry-ui.riot';
 

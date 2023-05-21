5a340291c2e010a6008cc986697c4b5989fe880c
Joxit <joxit972@gmail.com>
2023-05-21 10:13:59 +0200

    feat(catalog): add `CATALOG_DEFAULT_EXPANDED` to expand repositories by default (#302)
    closes #302

--- bin/90-docker-registry-ui.sh
+++ bin/90-docker-registry-ui.sh
@@ -12,6 +12,7 @@ sed -i "s~\${SHOW_CATALOG_NB_TAGS}~${SHOW_CATALOG_NB_TAGS}~" index.html
 sed -i "s~\${HISTORY_CUSTOM_LABELS}~${HISTORY_CUSTOM_LABELS}~" index.html
 sed -i "s~\${USE_CONTROL_CACHE_HEADER}~${USE_CONTROL_CACHE_HEADER}~" index.html
 sed -i "s~\${TAGLIST_ORDER}~${TAGLIST_ORDER}~" index.html
+sed -i "s~\${CATALOG_DEFAULT_EXPANDED}~${CATALOG_DEFAULT_EXPANDED}~" index.html
 
 grep -o 'THEME[A-Z_]*' index.html | while read e; do
   sed -i "s~\${$e}~$(printenv $e)~" index.html
